a9a5e892682567d3c1bfe2f499bcd15a4c9c4fd1
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-03-08 10:44:58 +0100

    unroll dcontract

--- src/symmetric_ops.jl
+++ src/symmetric_ops.jl
@@ -23,22 +23,51 @@ Base.issym(S::SymmetricTensors) = true
     @code :(return s)
 end
 
-function dcontract{dim, T1, T2}(S1::SymmetricTensor{4, dim, T1}, S2::SymmetricTensor{2, dim, T2})
-    r = SymmetricTensor{2, dim}(Am_mul_Bv(S1.data, S2.data))
-    scale_offdiagonal(r)
+@generated function dcontract{dim, T1, T2}(S1::SymmetricTensor{2, dim, T1}, S2::SymmetricTensor{4, dim, T2})
+    idx4(i,j,k,l) = compute_index(SymmetricTensor{4, dim}, i, j, k, l)
+    idx2(k,l) = compute_index(SymmetricTensor{2, dim}, k, l)
+    exps = Expr(:tuple)
+    for i in 1:dim, j in 1:i
+        exps_ele = Expr(:call)
+        push!(exps_ele.args, :+)
+        for k in 1:dim, l in 1:k
+            if k == l
+                push!(exps_ele.args, :(data4[$(idx4(k,l,i,j))] * data2[$(idx2(k,l))]))
+            else
+                push!(exps_ele.args, :( 2 * data4[$(idx4(k,l,i,j))] * data2[$(idx2(k,l))]))
+            end
+        end
+        push!(exps.args, exps_ele)
+    end
+    quote
+         data2 = S1.data
+         data4 = S2.data
+         @inbounds r = $exps
+         SymmetricTensor{2, dim}(r)
+    end
 end
 
-function dcontract{dim, T1, T2}(S1::SymmetricTensor{2, dim, T1}, S2::SymmetricTensor{4, dim, T2})
-    r = SymmetricTensor{2, dim}(Amt_mul_Bv(S1.data, S2.data))
-    scale_offdiagonal(r)
-end
-
-@generated function scale_offdiagonal{dim}(t::SymmetricTensor{2, dim})
-    g(i, dim) = is_diagonal_index(dim, i)? :(t.data[$i]) : :(2*t.data[$i])
-    f = (i) -> g(i, dim)
-    exp = tensor_create_elementwise(get_base(t), f)
-    return quote
-        typeof(t)($exp)
+@generated function dcontract{dim, T1, T2}(S1::SymmetricTensor{4, dim, T1}, S2::SymmetricTensor{2, dim, T2})
+    idx4(i,j,k,l) = compute_index(SymmetricTensor{4, dim}, i, j, k, l)
+    idx2(k,l) = compute_index(SymmetricTensor{2, dim}, k, l)
+    exps = Expr(:tuple)
+    for i in 1:dim, j in 1:i
+        exps_ele = Expr(:call)
+        push!(exps_ele.args, :+)
+        for k in 1:dim, l in 1:k
+            if k == l
+                push!(exps_ele.args, :(data4[$(idx4(i,j,k,l))] * data2[$(idx2(k,l))]))
+            else
+                push!(exps_ele.args, :( 2 * data4[$(idx4(i,j,k,l))] * data2[$(idx2(k,l))]))
+            end
+        end
+        push!(exps.args, exps_ele)
+    end
+    quote
+         data2 = S2.data
+         data4 = S1.data
+         @inbounds r = $exps
+         SymmetricTensor{2, dim}(r)
     end
 end
 
