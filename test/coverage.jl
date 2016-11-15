8d957c3721f9aac6fed6f6fa1e44f5a6a003cf7a
Fredrik Ekre <fredrik.ekre@chalmers.se>
2016-11-15 14:01:29 +0100

    temp disable docs

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,6 +1,6 @@
 # Only run coverage from linux nightly build on travis.
 get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.5" || exit()
 
 Pkg.add("Coverage")
 using Coverage
