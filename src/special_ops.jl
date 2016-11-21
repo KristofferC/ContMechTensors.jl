34652f45a054398b735f8b3afccf013f336151a3
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-11-21 09:12:09 +0100

    add tovoigt

--- src/special_ops.jl
+++ src/special_ops.jl
@@ -23,3 +23,46 @@ dotdot(::Vec, ::SymmetricFourthOrderTensor, ::Vec)
         Tensor{2, dim}(r)
     end
 end
+
+const VOIGT_ORDER = ([1;],
+                     [1 3;
+                      4  2],
+                     [1   6  5;
+                      9   2  4;
+                      8   7  3])
+
+###########
+# tovoigt #
+###########
+function tovoigt{dim, T}(t::Tensor{2, dim, T})
+    v = zeros(T, dim^2)
+    for i in 1:dim, j in 1:dim
+        v[VOIGT_ORDER[dim][i, j]] = t[i,j]
+    end
+    return v
+end
+
+function tovoigt{dim, T}(t::Tensor{4, dim, T})
+    v = zeros(T, dim^2, dim^2)
+    for i in 1:dim, j in 1:dim, k in 1:dim, l in 1:dim
+        v[VOIGT_ORDER[dim][i, j], VOIGT_ORDER[dim][k, l]] = t[i,j,k,l]
+    end
+    return v
+end
+
+function tovoigt{dim, T}(t::SymmetricTensor{2, dim, T})
+    v = zeros(T, n_components(SymmetricTensor{2, dim}))
+    for i in 1:dim, j in i:dim
+        v[VOIGT_ORDER[dim][i, j]] = t[i,j]
+    end
+    return v
+end
+
+function tovoigt{dim, T}(t::SymmetricTensor{4, dim, T})
+    nc = n_components(SymmetricTensor{2, dim})
+    v = zeros(T, nc, nc)
+    for i in 1:dim, j in i:dim, k in 1:dim, l in k:dim
+        v[VOIGT_ORDER[dim][i, j], VOIGT_ORDER[dim][k, l]] = t[i,j,k,l]
+    end
+    return v
+end
